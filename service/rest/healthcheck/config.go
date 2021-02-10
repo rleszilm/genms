c6d06b608dd5017a0dd385b8150ad37b0fe66f0c
Charles Zilm <ch@rleszilm.com>
2021-02-09 20:45:16 -0800

    fix env var name

--- service/rest/healthcheck/config.go
+++ service/rest/healthcheck/config.go
@@ -8,7 +8,7 @@ import (
 
 // Config is the struct used to parse configuration from environment variables.
 type Config struct {
-	Enabled       bool             `envconfig:"enable" default:"false"`
+	Enabled       bool             `envconfig:"enabled" default:"false"`
 	Name          string           `envconfig:"name" default:""`
 	RequestPrefix string           `envconfig:"request_path" default:"/health"`
 	HealthyFunc   http.HandlerFunc `ignored:"true"`
