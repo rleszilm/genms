5894653db214ff943a58e8bb1b313435368176e6
Charles Zilm <ch@rleszilm.com>
2021-06-12 10:30:47 -0700

    checkpoint

---
+++ dal/updater/config.go
@@ -0,0 +1,8 @@
+package updater
+
+type Config struct {
+	Database   string        `envconfig:"database" required:"true"`
+	Collection string        `envconfig:"collection" required:"true"`
+	Interval   time.Duration `envconfig:"interval" default:"15m"`
+	Timeout    time.Duration `envconfig:"timeout" default:"5s"`
+}
